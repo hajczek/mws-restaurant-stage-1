0bae05afc976a0fc1f62647add561b462447dd56
hajczek <i.hajczewska@gmail.com>
2018-04-24 13:01:24 +0200

    feat: add tabindex for elements of list of restaurants

--- js/main.js
+++ js/main.js
@@ -137,6 +137,7 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  li.setAttribute("tabindex", 1);
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
@@ -146,18 +147,22 @@ createRestaurantHTML = (restaurant) => {
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
+  name.setAttribute("tabindex", 1);
   li.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
+  neighborhood.setAttribute("tabindex", 1);
   li.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
+  address.setAttribute("tabindex", 1);
   li.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute("tabindex", 1);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
