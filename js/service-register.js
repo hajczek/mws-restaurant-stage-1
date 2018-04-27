83fe47ca3945be61f63fab88e62b9c51d7966b40
hajczek <i.hajczewska@gmail.com>
2018-04-27 09:20:24 +0200

    fix: add comment to code

--- js/service-register.js
+++ js/service-register.js
@@ -1,12 +1,13 @@
+// Register Service worker
 if ('serviceWorker' in navigator) {
 
   navigator.serviceWorker
     .register('./sw.js', { scope: './' })
     .then(function (registration) {
-      console.log("Service Worker registered");
+      console.log("Service worker is registered");
     })
-    .catch(function (errorr) {
-      console.log("Service Worker is not registered", errorr);
+    .catch(function (error) {
+      console.log("Service worker is not registered", error);
     })
 
 }
