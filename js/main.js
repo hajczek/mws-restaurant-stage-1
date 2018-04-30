bce36e308b555b3b00aa78879b222c97400c28e8
hajczek <i.hajczewska@gmail.com>
2018-04-30 07:51:10 +0200

    fix: change element h1 for element h2

--- js/main.js
+++ js/main.js
@@ -146,7 +146,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = 'Restaurant ' + restaurant.name + ' - photo';
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   name.setAttribute("tabindex", 0);
   li.append(name);
